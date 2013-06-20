0072c2e5a94f907abcddf2d2fa3737bad43b33fe
Zeromax <zero@brothers-project.de>
2013-06-20 22:38:05 +0200

    added database.sql and removed sql from tl_ce_section.php see #2

---
+++ config/database.sql
@@ -0,0 +1,21 @@
+-- ********************************************************
+-- *                                                      *
+-- * IMPORTANT NOTE                                       *
+-- *                                                      *
+-- * Do not import this file manually but use the Contao  *
+-- * install tool to create and maintain database tables! *
+-- *                                                      *
+-- ********************************************************
+
+--
+-- Table `tl_ce_section`
+--
+
+CREATE TABLE `tl_ce_section` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `tstamp` int(10) unsigned NOT NULL default '0',
+  `section` varchar(50) NOT NULL default '',
+  `contentElement` blob NULL,
+  `invisible` char(1) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
