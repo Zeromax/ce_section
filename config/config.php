77ff6977640ec7d014bb51cbf827e3cc35306713
Zeromax <zero@brothers-project.de>
2013-06-21 20:27:47 +0200

    added support for easy_themes see #5

--- config/config.php
+++ config/config.php
@@ -15,3 +15,12 @@
  */
 
 $GLOBALS['BE_MOD']['design']['themes']['tables'][] = "tl_ce_section";
+
+// Support extension easy_themes
+$GLOBALS['TL_EASY_THEMES_MODULES']['ce_section'] = array
+(
+	'title'         => $GLOBALS['TL_LANG']['tl_theme']['ce_section'][1],
+	'label'         => $GLOBALS['TL_LANG']['tl_theme']['ce_section'][0],
+	'href_fragment' => 'table=tl_ce_section',
+	'icon'          => 'system/modules/ce_section/assets/icon.gif'
+);
